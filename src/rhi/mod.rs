c0e3bbc25e1f2cabd4e3e307b5c880597610a199
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-07-13 13:02:34 -0400

    [rhi] Add re-exports

--- src/rhi/mod.rs
+++ src/rhi/mod.rs
@@ -4,3 +4,8 @@
 //! This interface also hides some of the less tasteful parts of the supported APIs, such as the explicit memory 
 //! management. The RHI will be implemented by at least Vulkan and Direct3D 12. I'd like to eventually also support 
 //! Metal, but there's a lot to do before then
+
+mod rhi;
+
+// Re-exports
+pub use rhi::*;
