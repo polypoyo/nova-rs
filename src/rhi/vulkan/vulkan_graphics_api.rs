9c4bd55f79fd4b4cf99bbf7992737bd3271e5a56
David Dubois <dexcelstraun7@gmail.com>
2019-07-14 10:36:28 -0700

    [rhi] Reformat project charter with thiccer line limit, and figuring out associative types

--- src/rhi/vulkan/vulkan_graphics_api.rs
+++ src/rhi/vulkan/vulkan_graphics_api.rs
@@ -1,11 +1,13 @@
-// use super::super::GraphicsApi;
+use super::super::GraphicsApi;
+
+use super::vulkan_physical_device::VulkanPhysicalDevice;
 
 pub struct VulkanGraphicsApi {}
 
-// impl GraphicsApi for VulkanGraphicsApi {
-//    type PhysicalDevice = VulkanPhysicalDevice;
-//
-//    fn get_adapters() -> Vec<VulkanPhysicalDevice> {
-//        unimplemented!()
-//    }
-//}
+impl GraphicsApi for VulkanGraphicsApi {
+    type PhysicalDevice = VulkanPhysicalDevice;
+
+    fn get_adapters() -> Vec<VulkanPhysicalDevice> {
+        unimplemented!()
+    }
+}
--- src/rhi/vulkan/vulkan_physical_device.rs
+++ src/rhi/vulkan/vulkan_physical_device.rs
@@ -1,19 +1,19 @@
-// use crate::rhi::*;
+use super::super::{DeviceCreationError, PhysicalDevice, PhysicalDeviceProperties};
 
-// pub struct VulkanPhysicalDevice {}
-//
-// impl PhysicalDevice for VulkanPhysicalDevice {
-//    type Device = ();
-//
-//    fn get_properties(&self) -> PhysicalDeviceProperties {
-//        unimplemented!()
-//    }
-//
-//    fn can_be_used_by_nova(&self) -> bool {
-//        unimplemented!()
-//    }
-//
-//    fn create_logical_device(&self) -> Result<Self::Device, DeviceCreationError> {
-//        unimplemented!()
-//    }
-//}
+pub struct VulkanPhysicalDevice {}
+
+impl PhysicalDevice for VulkanPhysicalDevice {
+    type Device = ();
+
+    fn get_properties(&self) -> PhysicalDeviceProperties {
+        unimplemented!()
+    }
+
+    fn can_be_used_by_nova(&self) -> bool {
+        unimplemented!()
+    }
+
+    fn create_logical_device(&self) -> Result<Self::Device, DeviceCreationError> {
+        unimplemented!()
+    }
+}
