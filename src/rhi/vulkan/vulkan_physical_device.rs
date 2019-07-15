8ba03c42d27afd9a7d7a1d19bd2a7589d01a94fe
David Dubois <dexcelstraun7@gmail.com>
2019-07-14 18:33:26 -0700

    [rhi::vulkan] Adding logic for checking if Nova can use a given physical device

--- src/rhi/vulkan/vulkan_physical_device.rs
+++ src/rhi/vulkan/vulkan_physical_device.rs
@@ -1,6 +1,29 @@
 use super::super::{DeviceCreationError, PhysicalDevice, PhysicalDeviceProperties};
 
-pub struct VulkanPhysicalDevice {}
+use ash::{extensions::khr::Swapchain, vk};
+
+pub struct VulkanPhysicalDevice {
+    instance: vk::Instance,
+
+    phys_device: vk::PhysicalDevice,
+}
+
+impl VulkanPhysicalDevice {
+    fn supports_needed_extensions() -> bool {
+        let available_extensions = match instance.enumerate_device_extension_properties(phys_device) {
+            Ok(extensions) => extensions,
+            Err(_) => Vec::new(),
+        };
+
+        let mut needed_extensions = get_needed_extensions()
+
+        for ext in available_extensions {
+            needed_extensions.remove(etx);
+        }
+
+        needed_extensions.is_empty()
+    }
+}
 
 impl PhysicalDevice for VulkanPhysicalDevice {
     type Device = ();
@@ -10,10 +33,34 @@ impl PhysicalDevice for VulkanPhysicalDevice {
     }
 
     fn can_be_used_by_nova(&self) -> bool {
-        unimplemented!()
+        let props = instance.get_physical_device_properties(phys_device);
+
+        if !supports_needed_extensions() {
+            return false;
+        }
+
+        true
     }
 
     fn create_logical_device(&self) -> Result<Self::Device, DeviceCreationError> {
         unimplemented!()
     }
+}
+
+#[cfg(all(unix, not(target_os = "android")))]
+fn get_needed_extensions() -> Vec<*const i8> {
+    vec![
+        vk::Surface::name().as_ptr(),
+        vk::XlibSurface::name().as_ptr(),
+        vk::DebugReport::name().as_ptr()
+    ]
+}
+
+#[cfg(windows)]
+fn get_needed_extensions() -> Vec<*const i8> {
+    vec![
+        vk::Surface::name().as_ptr(),
+        vk::Win32Surface::name().as_ptr(),
+        vk::DebugReport::name().as_ptr()
+    ]
 }
